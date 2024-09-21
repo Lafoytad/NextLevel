934c6c23f2264da5fb8d8a3ded28caed7dbb46c0
Matthew <wispmatvei@gmail.com>
2024-09-22 00:35:15 +0300

    add ContactForm

--- src/app/components/organisms/contact-form/contact-form.js
+++ src/app/components/organisms/contact-form/contact-form.js
@@ -1,16 +1,43 @@
+import Button from "../../atoms/button/button";
+import styles from "./contact-form.module.scss";
+
 export default function ContactForm() {
   return (
-    <section>
-      <h2></h2>
-      <form action="">
-        {/* flex */}
-        <textarea name="" id=""></textarea>
-        <div>
-          <input type="text" />
-          {/* кнопка с текстом */}
-          <button></button>
+    <section className={styles.section}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h2>Заполните форму</h2>
+          <form action="">
+            <textarea
+              className={styles.bodyText2}
+              placeholder="Напишите свой вопрос"
+              name=""
+              id=""
+            />
+            <div className={styles.column}>
+              <input
+                className={`${styles.bodyText2} ${styles.email}`}
+                placeholder="Введите e-mail"
+                type="text"
+              />
+              <div className={styles.wrap}>
+                <input type="checkbox" id="myCheckbox" />
+                <label className={styles.bodyText3} for="myCheckbox">
+                  Я ознакомлен(а) с{" "}
+                  <span className={styles.decoration}>
+                    политикой конфиденциальности
+                  </span>{" "}
+                  и согласен(на) на обработку{" "}
+                  <span className={styles.decoration}>
+                    персональных данных.
+                  </span>
+                </label>
+              </div>
+              <Button color={"#1d1d1b"}>Отправить</Button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
--- src/app/components/templates/simple-corporate-site/simple-corporate-site.js
+++ src/app/components/templates/simple-corporate-site/simple-corporate-site.js
@@ -12,7 +12,7 @@ export default function SimpleCorporateSite() {
       <RevenueVsUserSatisfaction />
       <OurMonetizationPartners />
       <DetailedReports />
-      {/* <ContactForm /> */}
+      <ContactForm />
     </div>
   );
 }
