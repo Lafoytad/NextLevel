39f69921205fe4c459a7efc8f59fe90c844c5f2f
Matthew <wispmatvei@gmail.com>
2024-09-21 23:14:47 +0300

    add DetailedReports

--- src/app/components/templates/simple-corporate-site/simple-corporate-site.js
+++ src/app/components/templates/simple-corporate-site/simple-corporate-site.js
@@ -11,7 +11,7 @@ export default function SimpleCorporateSite() {
       <HeroSection />
       <RevenueVsUserSatisfaction />
       <OurMonetizationPartners />
-      {/* <DetailedReports /> */}
+      <DetailedReports />
       {/* <ContactForm /> */}
     </div>
   );
